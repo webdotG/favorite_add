1f4b9f974d5ffb1733c2c7318c0382aeb60dd124
Grant Kirill <webdotg.inbox@gmail.com>
2023-11-17 00:13:28 +0300

    add base

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+
+import HomePage from './pages/home.page'
+import FavoritePage from './pages/favorite.page'
+
 import './App.module.scss'
+import Header from './AllOver/header/header'
 
 function App() {
 
   return (
     <>
-
+    <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/favorites' element={<FavoritePage />}/>
+      </Routes>
     </>
   )
 }
