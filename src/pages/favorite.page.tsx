1499de17fa4e40180b233057fef3e1a6ba8d1622
Grant Kirill <webdotg.inbox@gmail.com>
2023-11-17 21:53:07 +0300

    fix remove

--- src/pages/favorite.page.tsx
+++ src/pages/favorite.page.tsx
@@ -1,10 +1,28 @@
-
+import { useActions } from '../hooks/actions'
 import { useAppSelector } from '../hooks/redux'
+
 import style from './favoritepage.module.scss'
 
 function FavoritePage() {
-
+  const { removeFavorite } = useActions()
   const { favorites } = useAppSelector(state => state.github)
+
+
+  // const removeFromFavorite = (event: React.MouseEvent<HTMLButtonElement>) => {
+  //   event.preventDefault()
+  //   removeFavorite()
+  //   // setIsFavorite(!isFavorite)
+  // }
+  
+  const favoriteItem = favorites.map(f => (
+    <li className={style.favorite_item} key={f}>
+            <a href={f} target='blank'>{f}</a>
+            <button className={style.btn_favorite_remove}
+              onClick={() => removeFavorite(f)}>
+              REMOVE
+            </button>
+          </li>
+  )) 
 
   if (favorites.length === 0) {
     <p>no favorite</p>
@@ -13,14 +31,21 @@ function FavoritePage() {
   return (
     <div className={style.favorite_wrapper}>
       <ul className={style.favorite_list}>
-        {favorites.map(f => (
+        {favoriteItem}
+        {/* {favorites.map(f => (
           <li className={style.favorite_item} key={f}>
             <a href={f} target='blank'>{f}</a>
+            <button className={style.btn_favorite_remove}
+              onClick={removeFromFavorite}>
+              REMOVE
+            </button>
           </li>
-        ))}
+         ))}  */}
       </ul>
     </div>
   )
+
+  
 }
 
 export default FavoritePage
